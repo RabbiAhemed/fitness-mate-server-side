6676948c764881f91367c6b11f4ec1628f24a5b6
Rabbi Ahmed Limon <rabbiahemed>
2022-11-11 09:32:52 +0600

    dotenv in gitignore

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require("mongodb");
-
+require("dotenv").config();
 /* middle wares */
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  "mongodb+srv://<username>:<password>@cluster0.esmmy.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.esmmy.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
